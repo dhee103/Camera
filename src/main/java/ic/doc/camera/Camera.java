3c8d072b4d78b98a03eead32df64c81671a823d6
dhee <dsg115@ic.ac.uk>
2017-03-30 14:52:07 +0100

    modified pressShutter() to only write data when camera is on

--- src/main/java/ic/doc/camera/Camera.java
+++ src/main/java/ic/doc/camera/Camera.java
@@ -4,6 +4,7 @@ public class Camera {
 
     private final Sensor sensor;
     private MemoryCard memoryCard;
+    private boolean powerIsOn = false;
 
     public Camera(Sensor sensor, MemoryCard memoryCard) {
         this.sensor = sensor;
@@ -11,15 +12,19 @@ public class Camera {
     }
 
     public void pressShutter() {
-        memoryCard.write(sensor.readData());
+        if(powerIsOn) {
+            memoryCard.write(sensor.readData());
+        }
     }
 
     public void powerOn() {
         sensor.powerUp();
+        powerIsOn = true;
     }
 
     public void powerOff() {
         sensor.powerDown();
+        powerIsOn = false;
     }
 }
 
